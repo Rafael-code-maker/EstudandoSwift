c9731b3f23cdb4dbd75aa280e632e4f0d1bfdf53
Rafael Maia <rafaeldepaulamaia@yahoo.com.br>
2022-03-04 16:17:15 +0300

    Initial Commit

---
+++ JogoDaMemoria/JogoDaMemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogoDaMemoriaApp.swift
+//  JogoDaMemoria
+//
+//  Created by Admin on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct JogoDaMemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
